e5901d0767f5bbbf9355356fbde1575b6a86f4ef
Shirwac <87342392+shiwa4656@users.noreply.github.com>
2022-04-27 19:52:22 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+let weather = {
+   "apikey" :"b32835d6c5112bf6d89f0f4c72e02f0a",
+   fetchWeather: function (city) {
+       fetch(
+           "https://api.openweathermap.org/data/2.5/weather?q=" 
+           + city 
+           + "&units=metric&appid=" 
+           + this.apiKey
+        )
+           .then((response) => response.json())
+           .then((response) => console.log(data));
+
+
+
+   },
+   displayweather : function(data) {
+       const {name}= data;
+       const { icon, description } = data.weather[0];
+       const { temp, humidity} = data.main ;
+       const {speed} = data.wind;
+       console.log(name, icon, description, temp, humidity, speed);
+       document.querySelector(".city").innerText = " weather in" + name ;
+       document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+  icon + ".png";
+       document.querySelector(".description").innerText = description ;
+       document.querySelector(".temp").innerText = temp + "°C ";
+       document.querySelector(".humidity").innerText = "Humidity :" + humidity + "%";
+       document.querySelector(".windspeed").innerText = "Wind speed :" + speed +"Km/h";
+
+   },
+    search : function() {
+      this.fetchWeather(document.querySelector(".search-bar").value);
+
+    }
+};
+
+document.querySelector(".search button") .addEventsListener("click", function () {
+  weather.search();
+   
+
+});
